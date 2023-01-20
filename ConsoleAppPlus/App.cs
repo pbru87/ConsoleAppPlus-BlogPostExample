0f7034b142cf903d2e01735f6d5b4421646de58a
Patrick Brunck <info@patrick-brunck.de>
2023-01-20 16:37:31 +0100

    Added Dependency Injection (DI) and appsettings.json

---
+++ ConsoleAppPlus/App.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppPlus
+{
+    internal class App
+    {
+        #region Fields
+        private readonly IConfiguration _configuration;
+        #endregion
+
+        #region CTORs
+        public App(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        #endregion
+
+        #region Public Methods
+        public async Task Run(string[] args)
+        {
+            Console.WriteLine(_configuration["HelloWorld"]);
+            Console.WriteLine();
+            Console.WriteLine("Please look for 'ToDo' in the code to get started quickly with this project template!");
+            Console.ReadKey();
+        }
+        #endregion
+    }
+}
--- ConsoleAppPlus/Program.cs
+++ ConsoleAppPlus/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using SuWPB.DotNetUtils.Extensions;
+using ConsoleAppPlus;
+
+// Set up dependency injection
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    // Define entry point
+    await services.GetRequiredService<App>().Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+#region Local Methods
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    // ToDo: Uncomment if appsettings.json should be taken from another project/directory.
+    // Configure location of appsettings.json
+    //var workingDir = Directory.GetCurrentDirectory();
+    //var solutionDir = new DirectoryInfo(workingDir).GetParent(5)!.FullName;
+    //var appsettingsFolder = Path.Combine(solutionDir, "src", "<OTHER_PROJECT_FOLDER>");
+    //var fileProvider = new PhysicalFileProvider(appsettingsFolder);
+
+    // Configure HostBuilder
+    return Host.CreateDefaultBuilder(args)
+        // ToDo: Uncomment if appsettings.json should be taken from another project/directory.
+        //.ConfigureAppConfiguration((_, config) =>
+        //{
+        //    config.AddJsonFile(fileProvider, "appsettings.json", false, true);
+        //})
+        .ConfigureServices((_, services) =>
+        {
+            // ToDo: Register your required services here!
+            services.AddSingleton<App>();
+        });
+}
+#endregion
